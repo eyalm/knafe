74a97cda3c6b1f41ad7a53869678706ff0ae44a2
eyal.meltzer <eyal.meltzer@kaminario.com>
2019-10-17 16:14:32 +0300

    wip

--- pipline/demo_pipeline.groovy
+++ pipline/demo_pipeline.groovy
@@ -1,12 +1,5 @@
 #!/usr/bin/env groovy
 
-properties([
-  parameters([
-     booleanParam(name: 'DEPLOY_SHA', defaultValue: false),
-  ])
-])
-
-//def repo = "ssh://eyal.meltzer@bitbucket:7999/atf/k.n.a.f.e"
 def unittests
 
 node('katfv5') {
