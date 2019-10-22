17134e7fca223c8fbd63fcb7bbabc69e18dae56a
eyal.meltzer <eyal.meltzer@kaminario.com>
2019-10-22 10:25:14 +0300

    wip

--- pipeline/demo_pipeline.groovy
+++ pipeline/demo_pipeline.groovy
@@ -25,7 +25,7 @@ node('katfv5') {
         echo myPath
         sh "ls -la ${pwd()}"
         
-        def  FILES_LIST = sh (script: "ls   '${workers_dir}'", returnStdout: true).trim()
+        def  FILES_LIST = sh (script: "ls   '${pwd()}'", returnStdout: true).trim()
         echo "FILES_LIST : ${FILES_LIST}"
 
         unittests = load 'pipeline/pipeline.unittests'
