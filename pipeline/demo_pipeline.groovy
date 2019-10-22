152456a2cdaa9719ff4ae036d38abe6c627da16f
eyal.meltzer <eyal.meltzer@kaminario.com>
2019-10-22 10:02:05 +0300

    add pipeline to path for load

--- pipeline/demo_pipeline.groovy
+++ pipeline/demo_pipeline.groovy
@@ -21,7 +21,7 @@ node('katfv5') {
         deleteDir()
         //git branch: params.BRANCH, url: 'http://bitbucket/scm/atf/k.n.a.f.e.git'
         echo 'befor unitest load'
-        unittests = load 'pipeline_unittests.groovy'
+        unittests = load 'pipeline/pipeline_unittests.groovy'
         echo 'after unitest load'
 
     // Run UT
